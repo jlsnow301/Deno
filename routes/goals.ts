59baca337ca383ff6c31c7c93ef9cbd06adbe6a2
Jerm <42397676+jlsnow301@users.noreply.github.com>
2020-08-05 22:38:34 -0700

    New Goals Setup

--- routes/goals.ts
+++ routes/goals.ts
@@ -1,19 +1,39 @@
-import { Router } from "https://deno.land/x/oak/mod.ts";
+import { Router, HttpError, Status } from "https://deno.land/x/oak/mod.ts";
 import { renderFileToString } from "https://deno.land/x/dejs/mod.ts";
 
-const courseGoals: { name: string; id: string }[] = [];
+let courseGoals: { name: string; id: string }[] = [];
 
 const router = new Router();
 
+// READ
 router.get("/", async (ctx) => {
-  const body = await renderFileToString(Deno.cwd() + "/course_goals.ejs", {
-    title: "All Goals",
-    goals: courseGoals,
-  });
+  const body = await renderFileToString(
+    Deno.cwd() + "/views/course_goals.ejs",
+    {
+      title: "All Goals",
+      goals: courseGoals,
+    }
+  );
   ctx.response.body = body;
   //ctx.response.type = "text/html";
 });
 
+// READ - single element
+router.get("/:goalId", async (ctx) => {
+  const id = ctx.params.goalId;
+  const goal = courseGoals.find((goal) => goal.id === id);
+  if (!goal) {
+    const error = new HttpError();
+    error.status = Status.NotFound;
+    throw error;
+  }
+  const body = await renderFileToString(Deno.cwd() + "/views/course_goal.ejs", {
+    goal: goal,
+  });
+  ctx.response.body = body;
+});
+
+// CREATE
 router.post("/add-goal", async (ctx) => {
   const body = await ctx.request.body().value;
   const newGoalTitle = body.get(`new-goal`);
@@ -23,6 +43,28 @@ router.post("/add-goal", async (ctx) => {
   const newGoal = { id: new Date().toISOString(), name: newGoalTitle };
   courseGoals.push(newGoal);
   console.log(newGoal);
+  ctx.response.redirect("/");
+});
+
+// UPDATE
+router.post("/update-goal", async (ctx) => {
+  const body = await ctx.request.body().value;
+  const updatedGoalTitle = body.get(`goal-text`);
+  const updatedGoalId = body.get(`goal-id`);
+  const goal = courseGoals.find((goal) => goal.id === updatedGoalId);
+  if (!goal) {
+    const error = new HttpError();
+    error.status = Status.NotFound;
+    throw error;
+  }
+  goal.name = updatedGoalTitle;
+  ctx.response.redirect("/");
+});
+
+// DELETE
+router.post("/:goalId", async (ctx) => {
+  const id = ctx.params.goalId;
+  courseGoals = courseGoals.filter((goal) => goal.id !== id);
   ctx.response.redirect("/");
 });
 
